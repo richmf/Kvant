43cadde1afd130055ad28f432994035b141acb7a
Rich <rich@ciencias.unam.mx>
2020-10-07 17:19:08 -0500

    Tutorial Py3 actualizado

---
+++ Notebooks_Herramientas/Tutorial_Python3/Saludo_py3.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+def Saludar_desde_py3():
+    print('Hola, esta función vive en el archivo Saludo_py3.py')
+    
+# Archivo auxiliar del tutorial de Python 3.
+# Ir a las herramientas: http://sistemas.fciencias.unam.mx/~rich/Herramientas)
+# Curso relacionado con este notebook: [Física Computacional.](http://sistemas.fciencias.unam.mx/~rich/FisComp/)
+# Se agradece el apoyo de los proyectos DGAPA-PAPIME:
+# + PE 112919 durante el año 2020. *Mantenimiento general, actualización a la última versión de Python*
+# + PE 105017 durante el año 2017. *Idea original.*
