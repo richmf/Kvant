0ef23bacd6c87d4ee39a4fd99cfdda78160225b0
Rich <rich@ciencias.unam.mx>
2020-10-07 18:47:22 -0500

    Tutoriales actualizados

---
+++ Notebooks_Herramientas/Tutorial_Python2/Saludo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def Saludar_desde_py():
+    print 'Hola, esta función vive en el archivo Saludo.py'
+
+# Archivo auxiliar del tutorial de Python 2.
+# Ir a las herramientas: http://sistemas.fciencias.unam.mx/~rich/Herramientas)
+# Curso relacionado con este notebook: [Física Computacional.](http://sistemas.fciencias.unam.mx/~rich/FisComp/)
+# Se agradece el apoyo de los proyectos DGAPA-PAPIME:
+# + PE 112919 durante el año 2020. *Mantenimiento general, y ultima actualizacion de este tutorial*
+# + PE 105017 durante el año 2017. *Idea original.*
